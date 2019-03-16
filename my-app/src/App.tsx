3a6697c91e9534a5ab5d351e4702dbd22073a68a
syed <syed.abdullah@agoda.com>
2019-03-17 01:43:03 +0700

    dfasdfasdfadf

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  render() {ss
     return (
       <div className="App">
         <header className="App-header">
